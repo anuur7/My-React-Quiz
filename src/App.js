722b967299e9c34ab2d2fc072c1d1d0dced1bc3e
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-08-17 15:33:11 +0100

    displayed question and buttons on page

--- src/App.js
+++ src/App.js
@@ -1,48 +1,60 @@
-
-
 function App() {
   const questions = [
-		{
-			questionText: 'Which country did England play in the 1966 World Cup semi-finals? ?',
-			answerOptions: [
-				{ answerText: 'Portugal', isCorrect: true },
-				{ answerText: 'Spain', isCorrect: false },
-				{ answerText: 'Ukraine', isCorrect: false },
-				{ answerText: 'Germany', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many times has Liverpool won the UEFA Champions League (formerly known as the European Cup)?',
-			answerOptions: [
-				{ answerText: '2', isCorrect: false },
-				{ answerText: '6', isCorrect: true },
-				{ answerText: '7', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who won the Ballon dOr three times, in 1983, 1984 and 1985?',
-			answerOptions: [
-				{ answerText: 'Gary Lineker', isCorrect: false },
-				{ answerText: 'Diego Maradona', isCorrect: false },
-				{ answerText: 'Ian Rush', isCorrect: false },
-				{ answerText: 'Michel Platini	', isCorrect: true },
-			],
-		},
-		{
-			questionText: 'Which team has won the UEFA Champions League three times in a row, in 2016, 2017 and 2018',
-			answerOptions: [
-				{ answerText: 'Barcelona', isCorrect: false },
-				{ answerText: 'Bayern Munich', isCorrect: false },
-				{ answerText: 'Chelsea', isCorrect: false },
-				{ answerText: 'Real Madrid', isCorrect: true },
-			],
-		},
-	];
-
+    {
+      questionText:
+        "Which country did England play in the 1966 World Cup semi-finals?",
+      answerOptions: [
+        { answerText: "Portugal", isCorrect: true },
+        { answerText: "Spain", isCorrect: false },
+        { answerText: "Ukraine", isCorrect: false },
+        { answerText: "Germany", isCorrect: false },
+      ],
+    },
+    {
+      questionText:
+        "How many times has Liverpool won the UEFA Champions League (formerly known as the European Cup)?",
+      answerOptions: [
+        { answerText: "2", isCorrect: false },
+        { answerText: "6", isCorrect: true },
+        { answerText: "7", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+      ],
+    },
+    {
+      questionText:
+        "Who won the Ballon dOr three times, in 1983, 1984 and 1985?",
+      answerOptions: [
+        { answerText: "Gary Lineker", isCorrect: false },
+        { answerText: "Diego Maradona", isCorrect: false },
+        { answerText: "Ian Rush", isCorrect: false },
+        { answerText: "Michel Platini	", isCorrect: true },
+      ],
+    },
+    {
+      questionText:
+        "Which team has won the UEFA Champions League three times in a row, in 2016, 2017 and 2018",
+      answerOptions: [
+        { answerText: "Barcelona", isCorrect: false },
+        { answerText: "Bayern Munich", isCorrect: false },
+        { answerText: "Chelsea", isCorrect: false },
+        { answerText: "Real Madrid", isCorrect: true },
+      ],
+    },
+  ];
 
   return (
-    <div>
+    <div className="App">
+      <div className="question-section">
+        <div className="Question-Text">{questions[0].questionText}</div>
+      </div>
+      <div className="answer-section">
+        <div>
+          <button>Answer 1</button>
+          <button>Answer 2</button>
+          <button>Answer 3</button>
+          <button>Answer 4</button>
+        </div>
+      </div>
     </div>
   );
 }
